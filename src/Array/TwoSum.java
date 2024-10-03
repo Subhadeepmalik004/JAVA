9815535eef171c88034bf2d445193ea22f6aa204
Subhadeep Malik <subhadeepmalik004@gmail.com>
2024-10-04 02:36:13 +0530

    add leedcode Q.-1 Two Sum

---
+++ src/Array/TwoSum.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {3,-1,8,5,4,9,2};
+        int x = 9;
+        int n = arr.length;
+        // solution
+        for(int i=0; i<n; i++){
+            boolean flag = false;  // optimization
+            for(int j=i+1;j<n;j++){
+                if(arr[i] + arr[j] == x){
+                    System.out.println(arr[i] + " " + arr[j]);
+                    arr[0] = i;
+                    arr[1] = j;
+                    System.out.println("Array index: " + i +" " +j);
+
+                    flag = true;  // optimization
+                    break;  // optimization
+                }
+            }
+            if(flag == true) break;  // optimization
+        }
+    }
+}
