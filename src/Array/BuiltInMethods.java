c60b82bc56fea65e57eb517c9c3a30b55d32d8c6
Subhadeep Malik <subhadeepmalik004@gmail.com>
2024-10-03 20:34:16 +0530

    Array List In Java

---
+++ src/Array/BuiltInMethods.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Arrays;
+
+public class BuiltInMethods {
+    public static void main(String[] args) {
+        int[] arr = {30,10,40,23,89,34};
+//        for(int i=0;i<arr.length;i++){
+//            System.out.print(arr[i] + " ");
+//        }
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+        Arrays.sort(arr);
+        System.out.println();
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+//ele (x) -->> for int element (x) in array
---
+++ src/Array/CopyOfArray.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.util.Arrays;
+
+public class CopyOfArray {
+    public static void main(String[] args) {
+        int[] arr = {30,10,40,23,89,34};
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+//        int[] nums = arr;  // Shallow Copy
+////        for(int ele : nums){
+////            System.out.print(ele + " ");
+////        }
+//        nums[0] = 70;
+//        for(int ele : nums) {
+//            System.out.print(ele + " ");
+//        }
+//        System.out.println();
+        // Deep Copy
+        int[] brr = Arrays.copyOf(arr, arr.length);
+        for(int ele : brr){
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+
+        // Re-initialize of Deep Copy array
+        int[] crr = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            crr[i] = arr[i];
+        }
+        crr[0] = 100;
+        System.out.println(arr[0]);
+    }
+}
