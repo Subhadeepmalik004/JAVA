f77213b214fe4f8b13fc9c1785fd565161ec0cc3
Subhadeep Malik <subhadeepmalik004@gmail.com>
2024-11-04 22:58:50 +0530

    charAt() and length() charAt() -->> char index like Array length() -->> length of char

---
+++ src/strings/BuiltInStringMethods.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class BuiltInStringMethods {
+    public static void main(String[] args) {
+        String s = "Subhadeep Malik";
+        System.out.println(s.charAt(3));
+        System.out.println(s.charAt(9));
+        int n = s.length();
+        System.out.println(n);
+    }
+}
