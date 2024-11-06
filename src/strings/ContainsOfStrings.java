32bfb04e74a8c5cdcb50541045b1a498378c012c
Subhadeep Malik <subhadeepmalik004@gmail.com>
2024-11-06 21:18:47 +0530

    contains() and startsWith() and endsWith()

---
+++ src/strings/ContainsOfStrings.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.util.Scanner;
+
+public class ContainsOfStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        String s = sc.nextLine();
+        System.out.print("Enter the finding string : ");
+        String a = sc.nextLine();
+        System.out.println(s.contains(a));
+    }
+}
