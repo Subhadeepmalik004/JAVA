58f21050c14ab1c7d821ed56f7c3ee6f79d0f662
Subhadeep Malik <subhadeepmalik004@gmail.com>
2024-12-16 11:16:06 +0530

    BuiltInStringMethods update

--- src/strings/BuiltInStringMethods.java
+++ src/strings/BuiltInStringMethods.java
@@ -3,9 +3,9 @@ package strings;
 public class BuiltInStringMethods {
     public static void main(String[] args) {
         String s = "Subhadeep Malik";
-        System.out.println(s.charAt(3));
-        System.out.println(s.charAt(9));
+        System.out.println(s.charAt(3));  // h
+        System.out.println(s.charAt(9)); // space
         int n = s.length();
-        System.out.println(n);
+        System.out.println(n);  // 15
     }
 }
