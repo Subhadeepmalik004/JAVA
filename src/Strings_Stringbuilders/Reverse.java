9bf65c6530ec1c5753ff9335800a8dbf3bc737fb
Subhadeep Malik <subhadeepmalik004@gmail.com>
2024-12-18 22:07:31 +0530

    reverse()

---
+++ src/Strings_Stringbuilders/Reverse.java
@@ -0,0 +1,10 @@
+package Strings_Stringbuilders;
+
+public class Reverse {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("abcdef");
+        System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
